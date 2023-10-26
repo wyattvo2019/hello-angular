10058cd33af47ac7aad8a8a8ebf927199c1b4f40
Wyatt VO <votranquy96@gmail.com>
2023-10-27 01:06:15 +0700

    Create Product class

--- src/app/common/Product.ts
+++ src/app/common/Product.ts
@@ -1,5 +1,10 @@
-export interface Product {
-  name: string,
-  quantity: number,
-  price: number,
+export class Product {
+  productName: string;
+  productCode: string;
+  productPrice: number;
+  constructor(productName: string, productCode: string, productPrice: number){
+    this.productName = productName;
+    this.productCode = productCode;
+    this.productPrice = productPrice;
+  }
 }
