39a584676f6f159992069d2998d4b238c0441d0c
Wyatt VO <votranquy96@gmail.com>
2023-10-28 00:11:04 +0700

    Update app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { FormsModule } from '@angular/forms';
+import { StarRatingComponent } from './components/star-rating/star-rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductDetailComponent,
+    StarRatingComponent,
   ],
   imports: [
     BrowserModule,
