ecbf40e6d249052dcf2fce9b7c89ba1ef6852be3
Wyatt VO <votranquy96@gmail.com>
2023-10-27 15:34:31 +0700

    Add product detail component

---
+++ src/app/components/product-detail/product-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Product } from 'src/app/common/Product';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  product: Product;
+
+  constructor() {
+    this.product = new Product('','',0)
+  }
+
+  changeDetail(form: NgForm){
+    console.log(form.value);
+    this.product.productCode = form.value.productCode;
+    this.product.productCode = this.product.productCode.toUpperCase();
+  }
+
+  ngOnInit() {
+  }
+
+}
